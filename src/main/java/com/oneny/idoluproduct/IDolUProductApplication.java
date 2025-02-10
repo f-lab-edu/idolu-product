42da68fd1efa74412cb5dca80ed24d64dff3d4a5
oneny <gjwodnjs1996@gmail.com>
2025-02-10 20:08:24 +0900

    initial commit

---
+++ src/main/java/com/oneny/idoluproduct/IDolUProductApplication.java
@@ -0,0 +1,13 @@
+package com.oneny.idoluproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDolUProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IDolUProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneny/idoluproduct/IdoluProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneny.idoluproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdoluProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
